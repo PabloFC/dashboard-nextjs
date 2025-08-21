62a08a507345326362ec08a387c037c808af6b37
PabloFC <pablofc_68@hotmail.com>
2025-08-21 19:35:53 +0200

    utilizando useState para el contador

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,8 +1,30 @@
+"use client";
+
+import { useState } from "react";
 
 export default function CounterPage() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div>
-      <h1>Counter page</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito de compras</span>
+      <span className="text-9xl">{count}</span>
+
+      <div className="flex">
+        <button
+          className="flex items-center justify-center p-2 rounded-xl text-white bg-gray-900 hover:bg-gray-600 transition-all w[100px] mr-2"
+          onClick={() => setCount(count + 1)}
+        >
+          +1
+        </button>
+
+        <button
+          className="flex items-center justify-center p-2 rounded-xl text-white bg-gray-900 hover:bg-gray-600 transition-all w[100px] mr-2"
+          onClick={() => setCount(count - 1)}
+        >
+          -1
+        </button>
+      </div>
     </div>
   );
 }
